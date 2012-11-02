9ef38857f41bda7a5c950c7de2444b4fe7247a75
Cyril Lepagnot <cyril@lepagnot.fr>
2012-11-02 15:13:20 +0100

    add java xml security init

--- lib/ebics/base.rb
+++ lib/ebics/base.rb
@@ -3,3 +3,4 @@ require 'rubygems'
 require 'bundler/setup'
 require 'jbundler'
 Java::JavaSecurity::Security.addProvider Java::OrgBouncycastleJceProvider.BouncyCastleProvider.new
+Java::OrgApacheXmlSecurity.Init.init
