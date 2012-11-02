30ebb2f6c90fe8b4b15e2f3c332b9d3e0c638674
Cyril Lepagnot <cyril@lepagnot.fr>
2012-11-02 19:19:24 +0100

    fix file upload task

--- lib/ebics/file/upload.rb
+++ lib/ebics/file/upload.rb
@@ -8,14 +8,15 @@ module Ebics
         session.add_session_param 'FORMAT', 'pain.xxx.cfonb160.dct'
         session.add_session_param 'TEST', 'true'
         session.add_session_param 'EBCDIC', 'false'
+        session.product = product
 
         conf.trace_manager.trace_directory = conf.get_transfer_trace_directory(user)
 
         transfer_manager = Java::OrgKopiEbicsClient.FileTransfer.new(session)
-puts("\033[01;33m#{path.inspect}\033[0m")
-puts("\033[01;33m#{Java::OrgKopiEbicsSession.OrderType::FUL.inspect}\033[0m")
-puts("\033[01;33m#{Java::OrgKopiEbicsIo::IOUtils.get_file_content(path).inspect}\033[0m")
-        transfer_manager.send_file Java::OrgKopiEbicsIo::IOUtils.get_file_content(path), Java::OrgKopiEbicsSession.OrderType::FUL
+        transfer_manager.send_file(
+          Java::OrgKopiEbicsIo::IOUtils.get_file_content(path),
+          Java::OrgKopiEbicsSession.OrderType::FUL
+        )
       end
 
       def run(path, options)
